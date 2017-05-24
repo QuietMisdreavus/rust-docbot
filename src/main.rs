3df18fe22c0c566b8246b7c4cffe1f6e7c68d6fb
QuietMisdreavus <grey@quietmisdreavus.net>
2017-05-24 11:42:25 -0500

    move string searching into a sub-fn
    also improve "single path element" searches to not unwrap on get_def

--- src/main.rs
+++ src/main.rs
@@ -14,65 +14,73 @@ fn main() {
     println!("done!");
 
     while let Ok(input) = read_line() {
-        let input = input.trim();
-        if input.is_empty() {
+        if input.trim().is_empty() {
             break;
         }
 
-        let elems = input.split("::").collect::<Vec<_>>();
-        if elems.is_empty() {
-            break;
-        }
+        let def = find_def(&input, &host);
 
-        let name = elems.last().unwrap();
-
-        let search = host.search_for_id(name).unwrap();
-
-        if search.is_empty() {
-            println!("No results for \"{}\"", input);
-            continue;
-        }
-
-        let def = if elems.len() == 1 {
-            host.get_def(*search.first().unwrap()).unwrap()
+        if let Some(def) = def {
+            print_def(&def, &host);
         } else {
-            let mut def = None;
-
-            for result in &search {
-                let def_guess = host.get_def(*result).unwrap();
-
-                if let Some(p_id) = def_guess.parent {
-                    let parent = if let Ok(p) = host.get_def(p_id) {
-                        p
-                    } else {
-                        continue;
-                    };
-
-                    if Some(&*parent.name) == elems.iter().cloned().rev().skip(1).next() {
-                        def = Some(def_guess);
-                        break;
-                    }
-                } else {
-                    if elems.iter().cloned().rev().skip(1).next()
-                            .map(|p| def_guess.qualname.contains(p)).unwrap_or(false) {
-                        def = Some(def_guess);
-                        break;
-                    }
-                }
-            }
-
-            if let Some(def) = def {
-                def
-            } else {
-                println!("No results for \"{}\"", input);
-                continue;
-            }
-        };
-
-        print_def(&def, &host);
+            println!("No results for \"{}\"", input.trim());
+        }
     }
 
     println!("");
+}
+
+fn find_def(input: &str, host: &analysis::AnalysisHost) -> Option<analysis::Def> {
+    let input = input.trim();
+
+    let elems = input.split("::").collect::<Vec<_>>();
+    if elems.is_empty() {
+        return None;
+    }
+
+    let name = elems.last().unwrap();
+
+    let search = host.search_for_id(name).unwrap();
+
+    if search.is_empty() {
+        return None;
+    }
+
+    let mut def = None;
+
+    if elems.len() == 1 {
+        for result in &search {
+            if let Ok(res) = host.get_def(*result) {
+                def = Some(res);
+                break;
+            }
+        }
+    } else {
+        for result in &search {
+            let def_guess = host.get_def(*result).unwrap();
+
+            if let Some(p_id) = def_guess.parent {
+                let parent = if let Ok(p) = host.get_def(p_id) {
+                    p
+                } else {
+                    continue;
+                };
+
+                if Some(&*parent.name) == elems.iter().cloned().rev().skip(1).next() {
+                    def = Some(def_guess);
+                    break;
+                }
+            } else {
+                if elems.iter().cloned().rev().skip(1).next()
+                        .map(|p| def_guess.qualname.contains(p)).unwrap_or(false) {
+                    def = Some(def_guess);
+                    break;
+                }
+            }
+        }
+    }
+
+    def
 }
 
 fn print_def(def: &analysis::Def, host: &analysis::AnalysisHost) {
